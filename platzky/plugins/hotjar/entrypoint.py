6152e82c345c0ecbb8e4fd89fe23c94593e61477
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2025-01-27 20:25:35 +0100

    Insert hotjar code snippet

--- platzky/plugins/hotjar/entrypoint.py
+++ platzky/plugins/hotjar/entrypoint.py
@@ -1,30 +1,23 @@
 def process(app, plugin_config):
-    gtm_id = plugin_config["ID"]
+    hj_id = plugin_config["ID"]
 
     head_code = (
-        """<!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','"""
-        + gtm_id
-        + """');</script>
-        <!-- End Google Tag Manager -->
+        """<!-- Hotjar Tracking Code -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid: """
+                + hj_id
+                + """, hjsv: 6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+        <!-- End Hotjar Tracking Code -->
     """
     )
     app.add_dynamic_head(head_code)
-
-    body = (
-        """<!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id="""
-        + gtm_id
-        + """
-        "
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
-    """
-    )
-    app.add_dynamic_body(body)
 
     return app
